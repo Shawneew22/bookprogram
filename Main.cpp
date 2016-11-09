14fef019c88ac8cd3055a07414a88cae89a0d9d7
Shawnee <shawneew22@gmail.com>
2016-11-08 20:27:16 -0600

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -62,28 +62,86 @@ void DisplayBook(Book book)
 	cout << book.readingStatus << book.title << book.author << book.purchaseStatus << endl;
 }
 
+int GetValidInput(int min, int max)
+{
+	int choice;
+	cout << "Input >>" << endl;
+	cin >> choice;
 
+	while (choice < min || choice > max)
+	{
+		cout << "Please enter a valid choice: " << endl;
+		cin >> choice;
+	}
+	cin.ignore();
+	return choice;
+}
+
+void AddBookMenu(Book books[100], int& bookCount)
+{
+	string title;
+	string author;
+	int ps;
+	int rs;
+	cout << "Please enter a title: " << endl;
+	getline(cin, title);
+	cout << "Please enter an author: " << endl;
+	getline(cin, author);
+
+	cout << endl << "Purchase Status:" << endl;
+	cout << "\t" << OWNED << ". " << PurchaseStatusString(OWNED) << endl;
+	cout << "\t" << WISHLIST << ". " << PurchaseStatusString(WISHLIST) << endl;
+	ps = GetValidInput(OWNED, WISHLIST);
+	
+	cout << endl << "Reading Status:" << endl;
+	cout << "\t" << NOT_STARTED << ". " << ReadingStatus(NOT_STARTED) << endl;
+	cout << "\t" << READING << ". " << ReadingStatus(READING)<< endl;
+	cout << "\t" << FINISHED << ". " << ReadingStatus(FINISHED) << endl;
+	rs = GetValidInput(NOT_STARTED, FINISHED);
+
+	SetBook(books[bookCount], title, author, (PurchaseStatus)ps, (ReadingStatus)rs);
+	bookCount++;
+}
+
+void DisplayBookList(Book books[], int BookCount)
+{
+	for (int i = 0; i < BookCount; i++)
+	{
+		cout << i << ". ";
+		DisplayBook(books[i]);
+	}
+}
+
+void UpdateBookMenu(Book books[100], int& bookCount)
+{
+	cout << DisplayBookList << endl;
+	cout << "What would you like to update?" << endl;
+	int index;
+	cin << index;
+	index = GetValidInput(index, bookCount);
+	string title = books[index].title;
+	string author = books[index].author;
+	PurchaseStatus ps = books[index].purchaseStatus;
+	ReadingStatus rs = books[index].readingStatus;
+
+	cout << "1. Update title." << endl;
+	cout << "2. Update author." << endl;
+	cout << "3. Update purchase status." << endl;
+	cout << "4. Update reading status." << endl;
+
+	if (GetValidInput == 1)
+	{
+		cout << "Please enter Book title >>" << endl;
+		getline(cin, title);
+		SetBook(books[index], title, author, (PurchaseStatus)ps, (ReadingStatus)rs); //asdfasf
+}
 
 
 int main()
 {
-	Book bookA, bookB, bookC, bookD, bookE, bookF;
-
-	// Testing all permutations of purchase/reading status
-	SetBook(bookA, "Harry Potter and the Sorcerer's Stone", "J K Rowling", OWNED, FINISHED);
-	SetBook(bookB, "Who cooked the last supper?", "Rosalind Miles", OWNED, READING);
-	SetBook(bookC, "Marvirinstrato", "Tim Westover", OWNED, NOT_STARTED);
-
-	SetBook(bookD, "Masters of Doom", "David Kushner", WISHLIST, FINISHED);
-	SetBook(bookE, "Earthsong", "Suzette Haden Elgin", WISHLIST, READING);
-	SetBook(bookF, "Invent to Learn", "Martinez, Stager", WISHLIST, NOT_STARTED);
-
-	DisplayBook(bookA);
-	DisplayBook(bookB);
-	DisplayBook(bookC);
-	DisplayBook(bookD);
-	DisplayBook(bookE);
-	DisplayBook(bookF);
+	
+	Book books[100];
+	int bookCount = 0;
 
 	return 0;
 }
